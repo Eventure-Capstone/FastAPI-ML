78182c413dc181f4d5b174c0767902fe8b0d6994
Muhamamad Aris <mhdarisid@gmail.com>
2024-06-19 20:00:59 +0700

    Fix Port ENV

--- main.py
+++ main.py
@@ -330,5 +330,5 @@ async def get_categories():
         }
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
